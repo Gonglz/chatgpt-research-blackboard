7b8fb4b35ec584d626d4aa476c7db4083c22a098
Lz Gong <44239296+Gonglz@users.noreply.github.com>
2026-08-24 12:51:14 -0700

    fix: restore research highlight manager

--- src/sidepanel/index.jsx
+++ src/sidepanel/index.jsx
@@ -7,6 +7,7 @@ import App from './App';
 import AutoGraphDeltaBridge from './components/AutoGraphDeltaBridge';
 import DomGraphDeltaBridge from './components/DomGraphDeltaBridge';
 import ResearchConversationSyncBridge from './components/ResearchConversationSyncBridge';
+import ResearchHighlightManager from './components/ResearchHighlightManager';
 import ResearchLayoutBridge from './components/ResearchLayoutBridge';
 import ResearchProjectMirrorBridge from './components/ResearchProjectMirrorBridge';
 import SidecarPresenceBridge from './components/SidecarPresenceBridge';
@@ -70,6 +71,7 @@ function renderApp() {
       <ResearchConversationSyncBridge />
       <ResearchProjectMirrorBridge />
       <ResearchLayoutBridge />
+      <ResearchHighlightManager />
       <DomGraphDeltaBridge />
       {/* Legacy reader for existing v2 HTML-comment RGΔ stored in conversation data. */}
       <AutoGraphDeltaBridge />
